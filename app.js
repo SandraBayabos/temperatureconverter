9cc427371a22ac6c21a10a6640165c480fb1868c
Sandra Bayabos <sandra.bayabos@gmail.com>
2019-09-16 10:01:31 +0800

    created function to round numbers to 2dp

--- app.js
+++ app.js
@@ -2,28 +2,33 @@ const celciusInput = document.querySelector("#celcius > input");
 const farenheitInput = document.querySelector("#farenheit > input");
 const kelvinInput = document.querySelector("#kelvin > input");
 
+//takes in a number i.e. cTemp, fTemp, kTemp
+function roundNum(num) {
+  return Math.round(num * 100) / 100;
+}
+
 function celciusToFarenheitAndKelvin() {
   const cTemp = parseFloat(celciusInput.value);
   const fTemp = (cTemp * (9 / 5)) + 32;
   const kTemp = cTemp + 273.15;
-  farenheitInput.value = fTemp;
-  kelvinInput.value = kTemp;
+  farenheitInput.value = roundNum(fTemp);
+  kelvinInput.value = roundNum(kTemp);
 }
 
 function farenheitToCelciusAndKelvin() {
   const fTemp = parseFloat(farenheitInput.value);
   const cTemp = (fTemp - 32) * (5 / 9);
   const kTemp = (fTemp + 459.67) * 5 / 9;
-  celciusInput.value = cTemp;
-  kelvinInput.value = kTemp;
+  celciusInput.value = roundNum(cTemp);
+  kelvinInput.value = roundNum(kTemp);
 }
 
 function kelvinToCelciusAndFarenheit() {
   const kTemp = parseFloat(kelvinInput.value);
   const cTemp = kTemp - 273.15;
   const fTemp = 9 / 5 * (kTemp - 273) + 32;
-  celciusInput.value = cTemp;
-  farenheitInput.value = fTemp;
+  celciusInput.value = roundNum(cTemp);
+  farenheitInput.value = roundNum(fTemp);
 }
 
 celciusInput.addEventListener('input', celciusToFarenheitAndKelvin);
